efc1d835110d22fd51e7138836a9a04a646112aa
SJ9211 <jsj921127@naver.com>
2025-01-21 11:28:22 +0900

    Internal

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 ﻿internal class Program
 {
+    // Internal 같은 어셈블리에서만 접근가능
     private static void Main(string[] args)
     {
         Solution sol = new Solution();
         int[] intarray = new int[] { 1, 2, 100, -99, 1, 2, 3 };
         string STR = "happy birthday!";
         //Console.WriteLine(sol.Solution0121());
-        //Util.PrintIntArray(sol.Solution0121(intarray));
+        Util.PrintIntArray(sol.Solution0121(intarray));
         
         Sample sam = new Sample();
          sam.StringsSample();  
