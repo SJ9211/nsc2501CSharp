e7d27510f2d70564984059fe0c52d13ae4f14e58
SJ9211 <jsj921127@naver.com>
2025-01-22 10:25:30 +0900

    배열의 유사도

--- Solution.cs
+++ Solution.cs
@@ -4,11 +4,35 @@ using Microsoft.VisualBasic;
 class Solution
 {
      /// <summary>
+     /// 배열의 유사도
+     /// </summary>
+     /// <param name="s1"></param>
+     /// <param name="s2"></param>
+     /// <returns></returns>
+     public int Solution0122(string[] s1, string[] s2)
+     {
+          int answer = 0;
+          //  s1의 크기만큼 반복
+          foreach (var item1 in s1)
+          {
+               //  s2의 크기만큼 반복
+               foreach (var item2 in s2)
+               {
+                    //  같은게 없으면 answer ++
+                    if (item1 == item2)
+                    {
+                         answer++;
+                    }
+               }
+          }
+          return answer;
+     }
+     /// <summary>
      /// 배열 두 배 만들기
      /// </summary>
      /// <param name="numbers"></param>
      /// <returns></returns>
-    public int[] Solution0121(int[] numbers)
+     public int[] Solution0121(int[] numbers)
     {
           // numbers의 크기만큼 반복
           for(int i = 0; i < numbers.Length; i ++)
